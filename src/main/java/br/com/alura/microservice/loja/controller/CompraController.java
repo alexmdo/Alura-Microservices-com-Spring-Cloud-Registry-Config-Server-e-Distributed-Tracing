a7990e13680473ceb89b5a1bc05d207d604b58e4
Alexandre Oliveira <alexmdo@gmail.com>
2022-10-29 11:36:20 -0300

    Usando o RestTemplate

--- src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
+++ src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
@@ -1,6 +1,7 @@
 package br.com.alura.microservice.loja.controller;
 
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
+import br.com.alura.microservice.loja.service.CompraService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/compra")
 public class CompraController {
 
+    private CompraService compraService;
+
+    public CompraController(CompraService compraService) {
+        this.compraService = compraService;
+    }
+
     @RequestMapping(method = RequestMethod.POST)
     public void realizaCompra(@RequestBody CompraDTO compraDTO) {
-
+        compraService.realizaCompra(compraDTO);
     }
 
 }
