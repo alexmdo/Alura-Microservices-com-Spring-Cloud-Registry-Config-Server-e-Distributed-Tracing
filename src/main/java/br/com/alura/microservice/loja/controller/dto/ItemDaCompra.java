989db076feff4842ef53a547f92c0d9fe9383e0d
Alexandre Oliveira <alexmdo@gmail.com>
2022-10-29 00:19:15 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/microservice/loja/controller/dto/ItemDaCompra.java
@@ -0,0 +1,23 @@
+package br.com.alura.microservice.loja.controller.dto;
+
+public class ItemDaCompra {
+
+    private Long id;
+    private Integer quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
