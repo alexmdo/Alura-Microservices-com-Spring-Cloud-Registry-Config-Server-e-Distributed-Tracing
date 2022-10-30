24faba29800a6fa12b383b23968c701ce11eb755
Alexandre Oliveira <alexmdo@gmail.com>
2022-10-30 13:14:49 -0300

    Configurando Papertrail

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
@@ -2,6 +2,8 @@ package br.com.alura.microservice.fornecedor.controller;
 
 import br.com.alura.microservice.fornecedor.model.InfoFornecedor;
 import br.com.alura.microservice.fornecedor.service.InfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/info")
 public class InfoController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
 
     private InfoService infoService;
 
@@ -18,6 +22,7 @@ public class InfoController {
 
     @RequestMapping("/{estado}")
     public InfoFornecedor getInfoPorEstado(@PathVariable String estado) {
+        LOG.info("recebido pedido de informacoes do fornecedor de {}", estado);
         return infoService.getInfoPorEstado(estado);
     }
 
--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -5,12 +5,16 @@ import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoPedidoDTO;
 import br.com.alura.microservice.loja.model.Compra;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
 
     private FornecedorClient fornecedorClient;
 
@@ -19,7 +23,12 @@ public class CompraService {
     }
 
     public Compra realizaCompra(CompraDTO compraDTO) {
-        InfoFornecedorDTO infoPorEstado = fornecedorClient.getInfoPorEstado(compraDTO.getEndereco().getEstado());
+        String estado = compraDTO.getEndereco().getEstado();
+
+        LOG.info("buscando informacoes do fornecedor de {}", estado);
+        InfoFornecedorDTO infoPorEstado = fornecedorClient.getInfoPorEstado(estado);
+
+        LOG.info("realizando um pedido");
         InfoPedidoDTO infoPedidoDTO = fornecedorClient.realizaPedido(compraDTO.getItens());
 
         Compra compra = new Compra();
