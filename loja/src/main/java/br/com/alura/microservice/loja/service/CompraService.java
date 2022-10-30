03cc876f8ca1e3999aa8a88e944389e5deb2e556
Alexandre Oliveira <alexmdo@gmail.com>
2022-10-29 22:56:57 -0300

    Implementação com Spring Feign

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,26 +1,23 @@
 package br.com.alura.microservice.loja.service;
 
+import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
 
-    private RestTemplate client;
+    private FornecedorClient fornecedorClient;
 
-    public CompraService(RestTemplate client) {
-        this.client = client;
+    public CompraService(FornecedorClient fornecedorClient) {
+        this.fornecedorClient = fornecedorClient;
     }
 
     public void realizaCompra(CompraDTO compraDTO) {
-        ResponseEntity<InfoFornecedorDTO> infoFornecedorDTOResponseEntity = client
-                .exchange("http://fornecedor/info/" + compraDTO.getEndereco().getEstado(),
-                        HttpMethod.GET, null, InfoFornecedorDTO.class);
-
-        System.out.println(infoFornecedorDTOResponseEntity.getBody().getEndereco());
+        InfoFornecedorDTO infoPorEstado = fornecedorClient.getInfoPorEstado(compraDTO.getEndereco().getEstado());
+        System.out.println(infoPorEstado.getEndereco());
     }
 }
